634eb1485eb0266cd51939be9767a788c1de8ba8
Muhammed Anas <muhammadanaswayanad@gmail.com>
2024-11-29 11:48:22 +0530

    init

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Sandwich Leave",
+    "version": "1.0",
+    "summary": "Implements sandwich leave policy in Odoo Time Off module",
+    "description": "Deducts weekends and public holidays falling between leave days from the leave quota.",
+    "author": "Muhammed Anas",
+    "depends": ["hr_holidays"],
+    "data": [],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3"
+}
---
+++ models/hr_leave.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from datetime import timedelta
+from odoo import models, fields
+
+class HrLeave(models.Model):
+    _inherit = 'hr.leave'
+
+    sandwich_leave = fields.Boolean(string='Apply Sandwich Rule', default=True)
+
+    def _compute_duration(self):
+        for leave in self:
+            if leave.sandwich_leave:
+                # Get all dates in the leave period
+                leave_dates = set(
+                    leave.request_date_from + timedelta(days=i)
+                    for i in range((leave.request_date_to - leave.request_date_from).days + 1)
+                )
+                # Get non-working days (weekends and public holidays)
+                non_working_days = self.env['resource.calendar']._get_global_leaves(leave.employee_id.id)
+                # Calculate effective leave days
+                effective_days = leave_dates - non_working_days
+                leave.number_of_days = len(effective_days)
+            else:
+                super(HrLeave, self)._compute_duration()
